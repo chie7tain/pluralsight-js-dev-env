1e09ada94418d74189a88b32a440b285b6211cf0
Chie7tain <fredrickokwuobi@gmail.com>
2019-06-21 00:30:15 +0100

    changes made in several files

---
+++ buildScripts/startMessage.js
@@ -0,0 +1,3 @@
+import chalk from 'chalk';
+
+console.log(chalk.green('Starting app in dev mode yay...'));// eslint-disable-line no-console
---
+++ src/index.test.js
@@ -0,0 +1,24 @@
+		import jsdom from 'jsdom';
+		import fs from 'fs';
+		import {
+    expect
+    } from 'chai';
+
+    describe('Our first test', () => {
+    it('should pass', () => {
+    expect(true).to.equal(true);
+    });
+    });
+
+		describe('index.html', () => {
+			it('should have h1 that says their father left yansh', () => {
+				const index = fs.readFileSync('./src/index.html', "utf-8");
+				const {
+					JSDOM
+				} = jsdom;
+				const dom = new JSDOM(index);
+				const h1 = dom.window.document.getElementsByTagName("h1")[0];
+				expect(h1.innerHTML).to.equal("their father left yansh");
+				dom.window.close();
+			})
+		})
