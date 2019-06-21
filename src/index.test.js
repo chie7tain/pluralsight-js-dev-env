bec4eef7180fee99ba0c16753e1b722b6cf5a107
Chie7tain <fredrickokwuobi@gmail.com>
2019-06-21 01:03:18 +0100

    change h1 text to match path

--- src/index.test.js
+++ src/index.test.js
@@ -11,14 +11,14 @@
     });
 
 		describe('index.html', () => {
-			it('should have h1 that says their father left yansh', () => {
+			it('should have h1 that says welcome to Andela Ifeanyi', () => {
 				const index = fs.readFileSync('./src/index.html', "utf-8");
 				const {
 					JSDOM
 				} = jsdom;
 				const dom = new JSDOM(index);
 				const h1 = dom.window.document.getElementsByTagName("h1")[0];
-				expect(h1.innerHTML).to.equal("their father left yansh");
+				expect(h1.innerHTML).to.equal("welcome to Andela Ifeanyi");
 				dom.window.close();
 			})
 		})
